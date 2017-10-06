193570ee892ee60538c0a6b654312d7e73180718
Fábio Moreira <fabiomsnet@gmail.com>
2017-10-06 19:57:53 -0300

    Atualização exercicio 11.5

--- src/br/com/caelum/contas/ManipuladorDeTributaveis.java
+++ src/br/com/caelum/contas/ManipuladorDeTributaveis.java
@@ -1,10 +1,23 @@
 package br.com.caelum.contas;
 
+import br.com.caelum.contas.modelo.Tributavel;
 import br.com.caelum.javafx.api.util.Evento;
 
 public class ManipuladorDeTributaveis {
 	
+	private double total;
+	
 	public void calculaImpostos(Evento evento) {
-				
+		
+		total = 0;
+		int tamanho = evento.getTamanhoDaLista("listaTributaveis");
+		for (int i = 0; i < tamanho; i++) {
+			Tributavel t = evento.getTributavel("listaTributaveis", i);
+			total += t.getValorImposto();
+		}				
+	}
+	
+	public double getTotal() {
+		return total;		
 	}
 }
